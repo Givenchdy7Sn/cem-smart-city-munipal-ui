29e402d7615a723e2729dea51965eaf2a1d8e9ef
@mg.mojapelo <mg.mojapelo@gmail.com>
2026-05-09 11:47:33 +0200

    comments history endpoint

---
+++ src/app/models/correction-comment-history.model.ts
@@ -0,0 +1,10 @@
+export interface CorrectionCommentHistory {
+  id: number;
+  cremationBookingId: number;
+  correctionComments: string;
+  sentByName: string;
+  sentById: number;
+  sentDate: string;
+  bookingStatusAtTime: string;
+  createdAt: string;
+}
